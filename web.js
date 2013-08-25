5950db6c997aca1a830f82fcb841110453e86ff1
Anthony Akentiev <anthony.akentiev@gmai.com>
2013-08-25 20:58:07 +0000

    Added new directories for web-server processing

--- web.js
+++ web.js
@@ -19,6 +19,23 @@ app.get('/scripts/:doc',function(request, response) {
   response.sendfile('scripts/' + request.params.doc);
 });
 
+// TODO: fix that code
+app.get('/tests/:doc',function(request, response) {
+  response.sendfile('tests/' + request.params.doc);
+});
+
+app.get('/tests/lib/:doc',function(request, response) {
+  response.sendfile('tests/lib/' + request.params.doc);
+});
+
+app.get('/tests/spec/:doc',function(request, response) {
+  response.sendfile('tests/spec/' + request.params.doc);
+});
+
+app.get('/tests/lib/jasmine-1.3.1/:doc',function(request, response) {
+  response.sendfile('tests/lib/jasmine-1.3.1/' + request.params.doc);
+});
+
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
