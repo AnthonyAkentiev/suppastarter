0ca126559b4c9429938bf96f52929bd223dde5f7
Anthony Akentiev <anthony.akentiev@gmai.com>
2013-08-25 15:19:47 +0000

    Continued

--- web.js
+++ web.js
@@ -8,6 +8,16 @@ app.get('/', function(request, response) {
   response.send(buffer.toString('utf-8'));
 });
 
+app.get('/css/:doc',function(request, response) {
+  var buffer = fs.readFileSync('css/' + request.params.doc);
+  response.send(buffer.toString('utf-8'));
+});
+
+app.get('/scripts/:doc',function(request, response) {
+  var buffer = fs.readFileSync('scripts/' + request.params.doc);
+  response.send(buffer.toString('utf-8'));
+});
+
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
